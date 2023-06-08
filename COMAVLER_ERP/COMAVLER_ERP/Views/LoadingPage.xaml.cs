708f14fe6b6abca7ea3c2a524db9bdc79ac3bccb
HEEKWON <higun295@gmail.com>
2023-06-08 14:17:36 +0900

    20230608_2

---
+++ COMAVLER_ERP/COMAVLER_ERP/Views/LoadingPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace COMAVLER_ERP.Views;
+
+public partial class LoadingPage : ContentPage
+{
+	public LoadingPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ COMAVLER_ERP/COMAVLER_ERP/Views/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace COMAVLER_ERP.Views;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
